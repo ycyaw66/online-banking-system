4d443a7d38a1dc7e497eb31af6e75438fd5e4df9
dyxg <zxy.hhhh.cn@gmail.com>
2024-05-23 12:14:47 +0800

    entity update

--- backend/src/main/java/com/zjuse/bankingsystem/entity/Card.java
+++ backend/src/main/java/com/zjuse/bankingsystem/entity/Card.java
@@ -2,21 +2,19 @@ package com.zjuse.bankingsystem.entity;
 
 import com.baomidou.mybatisplus.annotation.TableName;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.zjuse.bankingsystem.utils.CardType;
 
 import io.micrometer.common.lang.NonNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
-
 @Data
-@TableName("blacklist")
+@TableName("card")
 @AllArgsConstructor
-public class Blacklist {
-    @JsonProperty("user_id")
+public class Card {
+    @JsonProperty("card_id")
+    Long cardId;
     @NonNull
-    Long userId;
-    @NonNull
-    String reason;
+    CardType cardType;
 }
--- backend/src/main/java/com/zjuse/bankingsystem/utils/CardType.java
+++ backend/src/main/java/com/zjuse/bankingsystem/utils/CardType.java
@@ -1,5 +1,5 @@
 package com.zjuse.bankingsystem.utils;
 
-public enum cardType { // 信用卡或储蓄卡
+public enum CardType { // 信用卡或储蓄卡
     CREDIT_CARD, DEBIT_CARD
 }
