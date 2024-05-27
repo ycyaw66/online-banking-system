04537f933527e784379aaad56ab2f4cd177026b3
dyxg <zxy.hhhh.cn@gmail.com>
2024-05-27 21:07:11 +0800

    jsonproperty of HistoryCondition

--- backend/src/main/java/com/zjuse/bankingsystem/entity/HistoryCondition.java
+++ backend/src/main/java/com/zjuse/bankingsystem/entity/HistoryCondition.java
@@ -2,15 +2,23 @@ package com.zjuse.bankingsystem.entity;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class HistoryCondition {
+    @JsonProperty("card_id")
     Long cardId;
+    @JsonProperty("target_card_id")
     Long targetCardId;
+    @JsonProperty("least_amount")
     BigDecimal LeastAmount;
+    @JsonProperty("most_amount")
     BigDecimal MostAmount;
+    @JsonProperty("start_time")
     Long startTime;
+    @JsonProperty("end_time")
     Long endTime;
     String remark;
 }
