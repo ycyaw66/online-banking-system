efc6b1903f1a8e40b44623334e7d41924fa9aadf
xftong <1290217090@qq.com>
2024-06-11 13:36:28 +0800

    更改侧边栏颜色

--- frontend/src/components/OfficerMainLayout.vue
+++ frontend/src/components/OfficerMainLayout.vue
@@ -101,7 +101,7 @@ export default {
 .aside {
   min-height: calc(100vh - 60px);
   width: 200px; /* 调整宽度 */
-  background-color: #520303; /* 深蓝色背景 */
+  background-color: #0270c1; /* 深蓝色背景 */
   color: white; /* 文本颜色为白色 */
   border-right: 1px solid #bdc3c7; /* 添加右侧边框 */
   box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1); /* 添加阴影 */
